b0d529eec6d291d893ad7f75c625ef9e2624f604
Ardila Javier <aj.ardila@transportsystems.co>
2020-05-28 18:38:57 -0500

    Enabling login users and remove all of AuthorizationServerConfig.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,6 @@ INSERT INTO authenticationConfig(name, value) VALUES ('config.access-token.valid
 INSERT INTO authenticationConfig(name, value) VALUES ('config.grant.types', 'password,client_credentials,refresh_token');
 INSERT INTO authenticationConfig(name, value) VALUES ('config.refresh-token.validity-seconds', '86400');
 INSERT INTO authenticationConfig(name, value) VALUES ('config.scope', 'WEB');
-INSERT INTO authenticationConfig (name, value) VALUES('config.otp-expiry-time', '1');
 
 INSERT INTO `role`(id, description, name) VALUES (1, 'Super user.', 'ROLE_ADMIN');
 INSERT INTO `role`(id, description, name) VALUES (2, 'Standard user.', 'ROLE_USER');
