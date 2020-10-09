8a21af8a3a55c5d128b5363c74ba89c37326d5f1
Ardila Javier <javier.ardila@flexibility.com.ar>
2020-10-08 22:32:33 -0500

    Bugs fixed.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,18 +4,20 @@ INSERT INTO authenticationConfig(name, value) VALUES ('config.grant.types', 'pas
 INSERT INTO authenticationConfig(name, value) VALUES ('config.refresh-token.validity-seconds', '86400');
 INSERT INTO authenticationConfig(name, value) VALUES ('config.scope', 'WEB');
 
--- INSERT INTO `role`(id, description, name) VALUES (1, 'Super user.', 'ROLE_ADMIN');
--- INSERT INTO `role`(id, description, name) VALUES (2, 'Standard user.', 'ROLE_BASIC');
---
--- INSERT INTO privilege(id, description, name) VALUES (1, 'User administrator privilege.', 'ADMIN_PRIVILEGE');
--- INSERT INTO privilege(id, description, name) VALUES (2, 'Standard user privilege.', 'BASIC_PRIVILEGE');
---
--- INSERT INTO role_privilege(role_id, privilege_id) VALUES (1, 1);
--- INSERT INTO role_privilege(role_id, privilege_id) VALUES (2, 2);
+INSERT INTO `role`(id, description, name) VALUES (1, 'Super user.', 'ROLE_ADMIN');
+INSERT INTO `role`(id, description, name) VALUES (2, 'Standard user.', 'ROLE_BASIC');
+INSERT INTO privilege(id, description, name) VALUES (1, 'User administrator privilege.', 'ADMIN_PRIVILEGE');
+INSERT INTO privilege(id, description, name) VALUES (2, 'Standard user privilege.', 'BASIC_PRIVILEGE');
+INSERT INTO role_privilege(role_id, privilege_id) VALUES (1, 1);
+INSERT INTO role_privilege(role_id, privilege_id) VALUES (2, 2);
 
 -- Credential for testing -> clientId: clientIdTest, clientSecret: admin
--- INSERT INTO credential(id, client_id, client_secret, created_at, credentials_expired, description, grant_type, `scope`) VALUES (1, 'clientIdTest', '$2a$10$Pz5yIaKO/JwGQ0pio1XLs.xDPrPv95SO0F2A3BCKV9USuD1xqUlZ.', '2020-04-10', 0, "Credential for testing.", 'password,client_credentials,refresh_token', 'WEB');
+INSERT INTO credential(id, client_id, client_secret, created_at, credentials_expired, description, grant_type, `scope`) VALUES (1, 'clientIdTest', '$2a$10$Pz5yIaKO/JwGQ0pio1XLs.xDPrPv95SO0F2A3BCKV9USuD1xqUlZ.', '2020-04-10', 0, "Credential for testing.", 'password,client_credentials,refresh_token', 'WEB');
 
--- Users for testing -> username: admin, password: ADM1n3$t123 & username: test, password: T3$t123abc
--- INSERT INTO `user` (account_locked, birth_date, created_at, document_number, email, email_verified, first_name, gender, last_access_at, last_name, password, phone_number, username, role_id) VALUES(0, '2020-05-23', '2020-05-23', '0000000000', 'admin@domain.co', 1, 'super', 'M', '2020-05-31 19:46:49.0', 'admin', '$2a$10$C4TpyN8Z2ea7znpYiWuSuu5rEUPAhfOcY9vCAJTLYH3u6ieuXTVG2', '00000000000', 'admin', 1);
--- INSERT INTO `user` (account_locked, birth_date, created_at, document_number, email, email_verified, first_name, gender, last_access_at, last_name, password, phone_number, username, role_id) VALUES(0, '2020-05-29', '2020-05-30', '0000000001', 'test@domain.co', 1, 'user', 'F', NULL, 'test', '$2a$10$1qcRVn5teIZ1IvtAvsfgsOyiv1xd08FbJxb2.WLlQrRgEbRpLfWka', '00000000001', 'test', 2);
+-- User for testing
+-- Username: admin
+-- Password: admin
+-- Client id: clientIdTest
+-- Client secret: admin
+INSERT INTO `user` (id, account_locked, created_at, email, email_verified, last_access_at, name, password, photo, username, role_id)
+VALUES(1, 0, '2020-10-08', 'renderjavi1998@gmail.com', 1, NULL, 'Test', '$2a$10$urm6t9kLlyMM0KoybwMGe.7gweYL64rOMbDNyWa/TGIz6jMTTaEw2', 'https://u-society.s3.amazonaws.com/1602210192210-image', 'admin', 2);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,53 @@ CREATE TABLE IF NOT EXISTS `authenticationConfig` (
   PRIMARY KEY (`name`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `privilege` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `role_privilege` (
+  `role_id` int(11) NOT NULL,
+  `privilege_id` int(11) NOT NULL,
+  FOREIGN KEY (`privilege_id`) REFERENCES `privilege` (`id`),
+  FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  PRIMARY KEY(`role_id`, `privilege_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `credential` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `client_id` varchar(255) NOT NULL UNIQUE,
+  `client_secret` varchar(255) NOT NULL,
+  `created_at` date NOT NULL,
+  `credentials_expired` tinyint(1) NOT NULL DEFAULT '0',
+  `description` varchar(255) DEFAULT NULL,
+  `grant_type` varchar(255) NOT NULL,
+  `scope` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_locked` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` date NOT NULL,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `email_verified` tinyint(1) NOT NULL DEFAULT '1',
+  `last_access_at` datetime DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL UNIQUE,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
