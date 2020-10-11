a42624b3075e8443d45bada2aa2dd4cb3df0eab5
Ardila Javier <javier.ardila@flexibility.com.ar>
2020-10-10 22:34:07 -0500

    Endpoint for updating user enabled.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,11 +13,3 @@ INSERT INTO role_privilege(role_id, privilege_id) VALUES (2, 2);
 
 -- Credential for testing -> clientId: clientIdTest, clientSecret: admin
 INSERT INTO credential(id, client_id, client_secret, created_at, credentials_expired, description, grant_type, `scope`) VALUES (1, 'clientIdTest', '$2a$10$Pz5yIaKO/JwGQ0pio1XLs.xDPrPv95SO0F2A3BCKV9USuD1xqUlZ.', '2020-04-10', 0, "Credential for testing.", 'password,client_credentials,refresh_token', 'WEB');
-
--- User for testing
--- Username: admin
--- Password: admin
--- Client id: clientIdTest
--- Client secret: admin
-INSERT INTO `user` (id, account_locked, created_at, email, email_verified, last_access_at, name, password, photo, username, role_id)
-VALUES(1, 0, '2020-10-08', 'renderjavi1998@gmail.com', 1, NULL, 'Test', '$2a$10$urm6t9kLlyMM0KoybwMGe.7gweYL64rOMbDNyWa/TGIz6jMTTaEw2', 'https://u-society.s3.amazonaws.com/1602210192210-image', 'admin', 2);
