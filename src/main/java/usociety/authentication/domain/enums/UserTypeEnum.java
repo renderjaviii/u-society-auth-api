9d06fc558f0cce19e7ca7d93ecf1d4dd5c72e90f
ajardila1 <aj.ardila@transportsystems.co>
2021-07-23 18:57:41 -0500

    User's types updated.

--- src/main/java/usociety/authentication/domain/enums/UserTypeEnum.java
+++ src/main/java/usociety/authentication/domain/enums/UserTypeEnum.java
@@ -2,23 +2,17 @@ package usociety.authentication.domain.enums;
 
 public enum UserTypeEnum {
 
-    ADMIN("admin", 1),
-    STANDARD("standard", 0);
+    ADMIN("ADMIN"),
+    STANDARD("BASIC");
 
     private String value;
-    private int code;
 
-    UserTypeEnum(String value, int code) {
+    UserTypeEnum(String value) {
         this.value = value;
-        this.code = code;
     }
 
     public String getValue() {
         return value;
-    }
-
-    public int getCode() {
-        return code;
     }
 
 }
